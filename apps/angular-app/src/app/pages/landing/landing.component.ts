24a908b1e9f8bb44f9aecf27081498c685cd1a2f
K1NG-Devops <king@youngeagles.org.za>
2025-05-04 00:10:51 +0200

    Wor

--- apps/angular-app/src/app/pages/landing/landing.component.ts
+++ apps/angular-app/src/app/pages/landing/landing.component.ts
@@ -2,14 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-landing',
-  imports: [],
   templateUrl: './landing.component.html',
-  styleUrl: './landing.component.css'
+  styleUrls: ['./landing.component.css']
 })
 export class LandingComponent {
 
-  redirectToReactApp() {
-    window.location.href = 'https://react-app-two-olive.vercel.app/';
+  redirectToReactApp(path: string = 'home'): void {
+    window.location.href = `https://youngeagles-proxy-iota.vercel.app/${path}`;
   }
 
 }
