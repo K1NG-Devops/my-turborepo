b46eb51b765ec1e72e57eacda99db0e770c215c9
K1NG-Devops <king@youngeagles.org.za>
2025-05-03 19:34:46 +0200

    Testing commits

---
+++ apps/angular-app/src/app/pages/galary/galary.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-galary',
+  imports: [],
+  templateUrl: './galary.component.html',
+  styleUrl: './galary.component.css'
+})
+export class GalaryComponent {
+
+}
