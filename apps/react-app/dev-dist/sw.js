2f8d30906e24e521b868101c3de158ca37855679
K1NG-Devops <61783617@mylife.unisa.ac.za>
2025-06-11 11:55:10 +0200

    Final commit for MVP

--- apps/react-app/dev-dist/sw.js
+++ apps/react-app/dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-3bc450b4'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.92sqbvvih5o"
+    "revision": "0.i96n8qddd3o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
